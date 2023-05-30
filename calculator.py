3dff92fa1218a52dde830e1d041b96d5cb5b3d05
Lynx-TP03 <134955517+Lynx-TP03@users.noreply.github.com>
2023-05-31 00:57:09 +0300

    Hesaplama Makinesi

---
+++ calculator.py
@@ -0,0 +1,50 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Cannot divide by zero"
+
+def calculator():
+    print("Welcome to the calculator!")
+    print("1. Toplama")
+    print("2. Çıkarma")
+    print("3. Çarpma")
+    print("4. Bölme")
+    print("5. Çıkış")
+
+    choice = input("Birini Seçin (1-5): ")
+
+    if choice == '5':
+        print("Exiting the calculator...")
+        return
+
+    num1 = float(input("İlk Sayıyı Giriniz: "))
+    num2 = float(input("İkinci Sayıyı Giriniz: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result:", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result:", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid choice. Please try again.")
+
+    calculator()
+
+calculator()
